6d572a951fdebe7fe258b36be89b2a8851d19e0a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-01-31 16:06:05 +0530

    surya: Use latest clang for compiling kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,6 +92,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 ifeq ($(TARGET_PREBUILT_KERNEL),)
   TARGET_KERNEL_CONFIG := surya_defconfig
   TARGET_KERNEL_CLANG_COMPILE := true
+  TARGET_KERNEL_USE_LATEST_CLANG := true
   TARGET_KERNEL_SOURCE := kernel/xiaomi/surya
 endif
 
