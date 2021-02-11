d323c89c82ebf3eebdf1f5fdd0b3a07db1ecb3ab
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-02-12 00:53:28 +0530

    surya: Remove unused WaveOS flags
     * we're now handling it differently

--- wave_surya.mk
+++ wave_surya.mk
@@ -9,7 +9,6 @@ $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common WaveOS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-WAVE_BUILD_TYPE := OFFICIAL
 $(call inherit-product, vendor/wave/configs/common.mk)
 
 # Device identifier. This must come after all inclusions.
