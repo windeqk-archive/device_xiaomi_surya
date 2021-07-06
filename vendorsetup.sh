12f414fef088f841c8276aaba123268912660c8d
windeqk <trojan.windows.11@gmail.com>
2021-07-06 21:51:38 +0200

    surya: Prepare vendorsetup.sh for faster build
    Add some lines for automate clone device repos

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+# Auto clone source
+git clone https://github.com/windeqk/device_xiaomi_sm6150-common -b p404-11.0 device/xiaomi/sm6150-common
+git clone https://github.com/windeqk/vendor_xiaomi_surya -b oss vendor/xiaomi/surya
+git clone https://github.com/windeqk/vendor_xiaomi_sm6150-common -b r vendor/xiaomi/sm6150-common --depth=1
+git clone https://github.com/ghostrider-reborn/android_kernel_xiaomi_surya -b LA.UM.9.x kernel/xiaomi/surya --depth=1
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-18.1 hardware/xiaomi --depth=1
