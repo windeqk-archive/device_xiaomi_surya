307048ec7be57559c8c7b05ad013de69d89bb571
windeqk <trojan.windows.11@gmail.com>
2021-07-11 20:36:22 +0200

    surya: Turn off remove folder
    [TEST COMMIT]
    It's only for faster build

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 # Display Hals
-rm -rf hardware/qcom-caf/sm8150/display
+#rm -rf hardware/qcom-caf/sm8150/display
 git clone https://github.com/PixelExperience/hardware_qcom-caf_sm8150_display hardware/qcom-caf/sm8150/display -b eleven --depth=1
 
 # Auto clone source
