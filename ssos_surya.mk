a7d358cd11ba7725d81deccc42250d777fb327c2
windeqk <trojan.windows.11@gmail.com>
2021-07-12 12:51:27 +0200

    surya: ShapeShiftOS Bringup

--- ssos_surya.mk
+++ ssos_surya.mk
@@ -16,19 +16,25 @@
 
 # Inherit framework first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit from common WaveOS configuration
-TARGET_BOOT_ANIMATION_RES := 1080
-$(call inherit-product, vendor/wave/configs/common.mk)
+# Inherit from common ShapeShiftOS configuration
+$(call inherit-product, vendor/ssos/config/common_full_phone.mk)
 
 # Device identifier
-PRODUCT_NAME := wave_surya
+PRODUCT_NAME := ssos_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# ShapeShiftOS stuff
+PRODUCT_PRODUCT_PROPERTIES += \
+  ro.ssos.cpu=SD732G
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_BLUR := true
