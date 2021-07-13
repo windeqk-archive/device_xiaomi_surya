40234eaf043407f45169a1c7723cd356c897d8e6
windeqk <trojan.windows.11@gmail.com>
2021-07-13 15:01:33 +0200

    surya: Drop vendorsetup.sh
    Reason:
    ignoring device/xiaomi/surya/vendorsetup.sh, not in vendor/ssos/allowed-vendorsetup_sh-files

--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-# Display Hals
-#rm -rf hardware/qcom-caf/sm8150/display
-git clone https://github.com/PixelExperience/hardware_qcom-caf_sm8150_display hardware/qcom-caf/sm8150/display -b eleven --depth=1
